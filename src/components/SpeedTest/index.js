e6d4c097b9bbd77775b1802fdaad679032043a50
burakozarslan <burakozarslan@icloud.com>
2021-06-29 02:51:34 +0100

    feat: Add typo tracking & Statistics
    After skipping to new word, number of either correct words or incorrect words gets incremented depending on if there is a typo or the current word is incompleted. This feature seems to be working, yet more testing is advised.

--- src/components/SpeedTest/index.js
+++ src/components/SpeedTest/index.js
@@ -15,23 +15,27 @@ const SpeedTest = () => {
   const [currentRowIndex, setCurrentRowIndex] = useState(0);
   // value entered in input field
   const [inputValue, setInputValue] = useState("");
-
-  const handleToNextWord = () => {
-    setCurrentWordIndex(currentWordIndex + 1);
-    setInputValue("");
-  };
-
-  const handleKeyPress = (event) => {
-    // if Spacebar pressed
-    if (event.nativeEvent.data === " ") handleToNextWord();
-    else setInputValue(event.target.value);
-  };
+  // holds state of current word's accuracy
+  const [isTypo, setIsTypo] = useState(false);
+  // number of correct words
+  const [numOfCorrectWords, setNumOfCorrectWords] = useState(0);
+  // number of incorrect words
+  const [numOfIncorrectWords, setNumOfIncorrectWords] = useState(0);
 
   useEffect(() => {
     fetchAllWords(40)
       .then(({ data }) => setWords(data))
       .catch((err) => console.log(err.message));
   }, []);
+
+  // updates isTypo state when inputValue changes
+  useEffect(() => {
+    if (words.length !== 0) {
+      // if current word and inputValue are equal
+      if (words[currentWordIndex] === inputValue) setIsTypo(false);
+      else setIsTypo(true);
+    }
+  }, [inputValue, currentWordIndex, words]);
 
   // executed after words are fetched
   useEffect(() => {
@@ -45,6 +49,25 @@ const SpeedTest = () => {
   useEffect(() => {
     setCurrentRowIndex(Math.floor(currentWordIndex / 8));
   }, [currentWordIndex]);
+
+  const handleToNextWord = () => {
+    setCurrentWordIndex(currentWordIndex + 1);
+    setInputValue("");
+
+    // increment correct words number by 1
+    if (isTypo) setNumOfCorrectWords(numOfCorrectWords + 1);
+    // increment incorrect words number by 1
+    else setNumOfIncorrectWords(numOfIncorrectWords + 1);
+
+    // reset isTypo state
+    setIsTypo(false);
+  };
+
+  const handleKeyPress = (event) => {
+    // if Spacebar pressed
+    if (event.nativeEvent.data === " ") handleToNextWord();
+    else setInputValue(event.target.value);
+  };
 
   return (
     <div className="speed-test--container">
