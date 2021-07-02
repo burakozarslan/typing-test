00e4ce16d1fbc9def38a841a559f0bc6d9c9b0d7
burakozarslan <burakozarslan@icloud.com>
2021-07-02 13:49:27 +0100

    Add styles for Result Card

--- src/components/ResultCard/index.js
+++ src/components/ResultCard/index.js
@@ -5,14 +5,17 @@ const ResultCard = (props) => {
   return (
     <div className="result-card__container">
       <div className="result-card__banner">
-        <div className="result-card__wpm">
-          <h1>{props.wpm} WPM</h1>
-          <p>(words per minute)</p>
-        </div>
-        <div className="result-card__statistics">
-          <h2>Correct Words: {props.correct}</h2>
-          <h2>Incorrect Words: {props.incorrect}</h2>
-        </div>
+        <h3>Result</h3>
+      </div>
+      <div className="result-card__wpm">
+        <h1>{props.wpm} WPM</h1>
+        <p>(words per minute)</p>
+      </div>
+      <div className="result-card__statistics">
+        {/* <h4>Key Strokes: NaN</h4>
+        <h4>Accuracy: NaN%</h4> */}
+        <h4>Correct Words: {props.correct}</h4>
+        <h4>Wrong Words: {props.incorrect}</h4>
       </div>
     </div>
   );
